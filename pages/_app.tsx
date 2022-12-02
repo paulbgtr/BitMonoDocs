ad1b920ac405b02139f6c72cb5de3952ba06e7e0
Gibsol <gibsol75@gmail.com>
2022-12-02 15:10:31 +0200

    App update Extended Chakra UI's theme to include a new color palette Styled body and Button component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,45 @@
-import "../styles/globals.css";
 import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#000",
+        color: "white",
+      },
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: "bold",
+        width: "140px",
+        borderRadius: "13px",
+        transitionDuration: "0.3s",
+        _hover: {
+          opacity: 0.8,
+        },
+      },
+      variants: {
+        dark: {
+          bg: "#000",
+          color: "#fff",
+          height: "50px",
+        },
+        light: {
+          bg: "#fff",
+          color: "#000",
+          height: "50px",
+        },
+      },
+    },
+  },
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
