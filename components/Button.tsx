6f950cd26aaa82fea124364ba8af37b55e9eb292
Gibsol <gibsol75@gmail.com>
2022-12-02 16:04:34 +0200

    Added Button component Added Button component that extends functionality of the Chakra's Button component: It adds routing to the /download page. It's a work in progress and will be improved in the future with a variable of path to a page.

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+import { Button as ChakraButton, Link } from "@chakra-ui/react";
+import { FC } from "react";
+
+interface ButtonProps {
+  href: string;
+  title: string;
+}
+
+const Button: FC<ButtonProps> = (props) => {
+  const router = useRouter();
+  return (
+    <ChakraButton
+      onClick={() => {
+        router.push("/download");
+      }}
+    >
+      {props.title}
+    </ChakraButton>
+  );
+};
+
+export default Button;
