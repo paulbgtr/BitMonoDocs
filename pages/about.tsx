f6b3b015e12c5356227cd3061b6e21c1e2c48065
Gibsol <gibsol75@gmail.com>
2022-12-06 19:49:30 +0200

    Added a new page to the project: About. This is a simple page that will be used to display information about the project.

---
+++ pages/about.tsx
@@ -0,0 +1,124 @@
+import Head from "next/head";
+import {
+  Container,
+  Heading,
+  Text,
+  UnorderedList,
+  ListItem,
+  Link,
+} from "@chakra-ui/react";
+
+const downloadLink = () => {
+  window.location.href =
+    " https://sourceforge.net/projects/bitmono/files/v0.3.3-alpha.8/";
+};
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Bitmono | About</title>
+        <meta
+          name="description"
+          content="An open-source, free protector for Mono"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Container maxWidth={"3xl"} mt={[50, 140]}>
+          <Heading as={"h1"} mb={[2, 3]} fontSize={[50, 64]} variant={"h1"}>
+            About
+          </Heading>
+
+          <Heading
+            as={"h2"}
+            fontSize={[30, 34]}
+            mb={[2, 3]}
+            mt={[3, 4]}
+            variant={"h2"}
+          >
+            What is BitMono?
+          </Heading>
+
+          <Text fontSize={[18, 20]} mb={[2, 3]} variant={"p"}>
+            Bit Mono is a free open-source C# obfuscator that in most cases
+            works only with Mono - well known as a fork of .NET Framework but
+            with custom bugs or Unity. Which uses its own fork of dnlib for
+            assembly manipulation. If you have any questions/issues please let
+            me know there. You can install latest version of Bit Mono here.
+          </Text>
+
+          <Heading
+            as={"h2"}
+            fontSize={[30, 34]}
+            mb={[2, 3]}
+            mt={[3, 4]}
+            variant={"h2"}
+          >
+            How your app will look since BitMono obfuscation - just in a few
+            words
+          </Heading>
+
+          <UnorderedList fontSize={[18, 20]} mb={[2, 3]}>
+            <ListItem>
+              Seems to C++ application but this is an actual C# application
+            </ListItem>
+            <ListItem>Crash of decompilers when analyzing types</ListItem>
+            <ListItem>Broken decompilers</ListItem>
+            <ListItem>Broken IL Code</ListItem>
+            <ListItem>Invisible types</ListItem>
+            <ListItem>No code</ListItem>
+          </UnorderedList>
+
+          <Heading
+            as={"h2"}
+            fontSize={[30, 34]}
+            mb={[2, 3]}
+            mt={[3, 4]}
+            variant={"h2"}
+          >
+            Obfuscation Features
+          </Heading>
+
+          <UnorderedList fontSize={[18, 20]} mb={[2, 3]}>
+            <ListItem>StringsEncryption</ListItem>
+            <ListItem>
+              <Link
+                color={"blue.500"}
+                href="https://github.com/0x59R11/BitDotNet"
+              >
+                BitDotNet
+              </Link>{" "}
+              (based and improved on existing protection)
+            </ListItem>
+            <ListItem>
+              <Link
+                color={"blue.500"}
+                href="https://github.com/sunnamed434/BitMethodDotnet"
+              >
+                BitMethodDotnet
+              </Link>{" "}
+              (based and improved on existing protection)
+            </ListItem>
+            <ListItem>
+              <Link
+                color={"blue.500"}
+                href="https://github.com/Elliesaur/DotNetHook"
+              >
+                DotNetHook
+              </Link>{" "}
+              (based and improved on existing protection)
+            </ListItem>
+            <ListItem>Call to call</ListItem>
+            <ListItem>FieldsHiding (Deprecated)</ListItem>
+            <ListItem>ObjecctReturnType</ListItem>
+            <ListItem>NoNamespaces</ListItem>
+            <ListItem>FullRenamer</ListItem>
+            <ListItem>AntiDebugBreakpoints</ListItem>
+          </UnorderedList>
+        </Container>
+      </main>
+    </>
+  );
+}
