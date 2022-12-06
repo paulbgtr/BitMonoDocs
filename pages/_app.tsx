03244ff5363410f43477717a6a9e9f86e0660c12
Gibsol <gibsol75@gmail.com>
2022-12-06 19:43:32 +0200

    Theme and Navbar Separated the theme into its own file and imported it into the _app.tsx file. Also added a navbar component and a few pages.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,64 +1,12 @@
 import { AppProps } from "next/app";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        bg: "#000",
-        color: "white",
-      },
-      a: {
-        color: "blue.500",
-        _hover: {
-          textDecoration: "underline",
-        },
-      },
-    },
-  },
-  components: {
-    Heading: {
-      variants: {
-        h1: {
-          fontWeight: "bold",
-          lineHeight: "1.2",
-          letterSpacing: "-0.02em",
-        },
-        h2: {
-          fontWeight: "0",
-          lineHeight: "1.2",
-          letterSpacing: "-0.02em",
-        },
-      },
-    },
-    Button: {
-      baseStyle: {
-        fontWeight: "bold",
-        borderRadius: "13px",
-        transitionDuration: "0.3s",
-        _hover: {
-          opacity: 0.8,
-        },
-      },
-      variants: {
-        dark: {
-          bg: "#000",
-          color: "#fff",
-          height: "50px",
-        },
-        light: {
-          bg: "#fff",
-          color: "#000",
-          height: "50px",
-        },
-      },
-    },
-  },
-});
+import Navbar from "../components/Navbar";
+import theme from "../utils/theme";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ utils/theme.ts
@@ -0,0 +1,73 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#000",
+        color: "white",
+      },
+      a: {
+        color: "blue.500",
+        _hover: {
+          textDecoration: "underline",
+        },
+      },
+    },
+  },
+  components: {
+    Heading: {
+      variants: {
+        h1: {
+          fontWeight: "bold",
+          lineHeight: "1.2",
+          letterSpacing: "-0.02em",
+        },
+        h2: {
+          fontWeight: "0",
+          lineHeight: "1.2",
+          letterSpacing: "-0.02em",
+        },
+      },
+    },
+    Link: {
+      baseStyle: {
+        fontWeight: "bold",
+        color: "blue.500",
+      },
+      variants: {
+        navLink: {
+          color: "white",
+          fontWeight: "0",
+          _hover: {
+            textDecoration: "none",
+          },
+        },
+      },
+    },
+    Button: {
+      baseStyle: {
+        fontWeight: "bold",
+        borderRadius: "13px",
+        transitionDuration: "0.3s",
+        _hover: {
+          opacity: 0.8,
+        },
+      },
+      variants: {
+        dark: {
+          bg: "#000",
+          color: "#fff",
+          height: "50px",
+        },
+        light: {
+          bg: "#fff",
+          color: "#000",
+          height: "50px",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
