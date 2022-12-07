d312a16b486eaeefe8fbc909fa5c02adb804a6b1
Gibsol <gibsol75@gmail.com>
2022-12-07 21:21:28 +0200

    _app update Added margin-bottom 10 globally to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import theme from "../utils/theme";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
-      <Component {...pageProps} />
+      <Box mb={10}>
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   );
 }
