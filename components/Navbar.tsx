ad515b11ab3dda175f10036b1613d1e67f14840d
Gibsol <gibsol75@gmail.com>
2022-12-12 07:04:21 +0200

    Navbar fix Aligned hamburger menu position to the right and brand to the left. Aligned hamburger menu's items to the right.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,6 +33,15 @@ function Navbar() {
   return (
     <Box bg={"#000"} px={4}>
       <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <HStack spacing={8}>
+          <Link variant={"navLink"} href={"/"}>
+            <Box>Bitmono</Box>
+          </Link>
+          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+            <NavbarLink href={links["about"]} title={"About"} />
+            <NavbarLink href={links["documentation"]} title={"Documentation"} />
+          </HStack>
+        </HStack>
         <IconButton
           size={"md"}
           icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -42,20 +51,11 @@ function Navbar() {
           colorScheme={"black"}
           color={"white"}
         />
-        <HStack spacing={8} alignItems={"center"}>
-          <Link variant={"navLink"} href={"/"}>
-            <Box>Bitmono</Box>
-          </Link>
-          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
-            <NavbarLink href={links["about"]} title={"About"} />
-            <NavbarLink href={links["documentation"]} title={"Documentation"} />
-          </HStack>
-        </HStack>
       </Flex>
 
       {isOpen ? (
         <Box pb={4} display={{ md: "none" }}>
-          <Stack as={"nav"} spacing={4}>
+          <Stack as={"nav"} display={"flex"} alignItems={"end"} spacing={4}>
             <NavbarLink href={links["about"]} title={"About"} />
             <NavbarLink href={links["documentation"]} title={"Documentation"} />
           </Stack>
