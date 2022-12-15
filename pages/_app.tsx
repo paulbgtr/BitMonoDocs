ea77fbde2846a8316dc5acdcf799f5c5137e9300
Gibsol <gibsol75@gmail.com>
2022-12-15 19:35:14 +0200

    chakraTheme renamed the imported 'theme' to chakraTheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
-import theme from "../utils/theme";
+import chakraTheme from "../utils/chakraTheme";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={chakraTheme}>
       <Navbar />
       <Box mb={10}>
         <Component {...pageProps} />
