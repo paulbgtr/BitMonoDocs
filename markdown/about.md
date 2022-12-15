3e7c3c21fbb9f191be045293cc5577fd1e84ff61
Gibsol <gibsol75@gmail.com>
2022-12-15 19:30:53 +0200

    MarkDown support for the About page

---
+++ markdown/about.md
@@ -0,0 +1,30 @@
+# About
+
+## What is Bitmono?
+
+Bit Mono is a free open-source C# obfuscator that in most cases
+works only with Mono - well known as a fork of .NET Framework but
+with custom bugs or Unity. Which uses its own fork of dnlib for
+assembly manipulation. If you have any questions/issues please let
+me know there. You can install latest version of Bit Mono here.
+
+## How your app will look since BitMono obfuscation - just in a few words
+
+- Seems to C++ application but this is an actual C# application
+- Crash of decompilers when analyzing types
+- Broken decompilers
+- Broken IL Code
+- Invisible types
+- No code
+
+## Obfuscation Features
+
+- [StringsEncryption ](https://github.com/0x59R11/BitDotNet)(based and improved on existing protection)
+- [BitMethodDotnet ](https://github.com/sunnamed434/BitMethodDotnet)(based and improved on existing protection)
+- [DotNetHook](https://github.com/Elliesaur/DotNetHook)(based and improved on existing protection)
+- Call to calli
+- FieldsHiding (Deprecated)
+- ObjecctReturnType
+- NoNamespaces
+- FullRenamer
+- AntiDebugBreakpoints
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,8 @@
 // todo: Convert this into a Markdown file
 
+import Markdown from "../markdown/about.md";
 import Head from "next/head";
-import {
-  Container,
-  Heading,
-  Text,
-  UnorderedList,
-  ListItem,
-  Link,
-} from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 
 const About = () => {
   return (
@@ -24,96 +18,7 @@ const About = () => {
 
       <main>
         <Container maxWidth={"3xl"}>
-          <Heading as={"h1"} mb={4} fontSize={[50, 64]} variant={"h1"}>
-            About
-          </Heading>
-
-          <Heading
-            as={"h2"}
-            fontSize={[30, 34]}
-            mb={[2, 3]}
-            mt={[3, 4]}
-            variant={"h2"}
-          >
-            What is BitMono?
-          </Heading>
-
-          <Text fontSize={[18, 20]} mb={[2, 3]} variant={"p"}>
-            Bit Mono is a free open-source C# obfuscator that in most cases
-            works only with Mono - well known as a fork of .NET Framework but
-            with custom bugs or Unity. Which uses its own fork of dnlib for
-            assembly manipulation. If you have any questions/issues please let
-            me know there. You can install latest version of Bit Mono here.
-          </Text>
-
-          <Heading
-            as={"h2"}
-            fontSize={[30, 34]}
-            mb={[2, 3]}
-            mt={[3, 4]}
-            variant={"h2"}
-          >
-            How your app will look since BitMono obfuscation - just in a few
-            words
-          </Heading>
-
-          <UnorderedList fontSize={[18, 20]} mb={[2, 3]}>
-            <ListItem>
-              Seems to C++ application but this is an actual C# application
-            </ListItem>
-            <ListItem>Crash of decompilers when analyzing types</ListItem>
-            <ListItem>Broken decompilers</ListItem>
-            <ListItem>Broken IL Code</ListItem>
-            <ListItem>Invisible types</ListItem>
-            <ListItem>No code</ListItem>
-          </UnorderedList>
-
-          <Heading
-            as={"h2"}
-            fontSize={[30, 34]}
-            mb={[2, 3]}
-            mt={[3, 4]}
-            variant={"h2"}
-          >
-            Obfuscation Features
-          </Heading>
-
-          <UnorderedList fontSize={[18, 20]} mb={[2, 3]}>
-            <ListItem>StringsEncryption</ListItem>
-            <ListItem>
-              <Link
-                color={"blue.500"}
-                href="https://github.com/0x59R11/BitDotNet"
-              >
-                BitDotNet
-              </Link>{" "}
-              (based and improved on existing protection)
-            </ListItem>
-            <ListItem>
-              <Link
-                color={"blue.500"}
-                href="https://github.com/sunnamed434/BitMethodDotnet"
-              >
-                BitMethodDotnet
-              </Link>{" "}
-              (based and improved on existing protection)
-            </ListItem>
-            <ListItem>
-              <Link
-                color={"blue.500"}
-                href="https://github.com/Elliesaur/DotNetHook"
-              >
-                DotNetHook
-              </Link>{" "}
-              (based and improved on existing protection)
-            </ListItem>
-            <ListItem>Call to calli</ListItem>
-            <ListItem>FieldsHiding (Deprecated)</ListItem>
-            <ListItem>ObjecctReturnType</ListItem>
-            <ListItem>NoNamespaces</ListItem>
-            <ListItem>FullRenamer</ListItem>
-            <ListItem>AntiDebugBreakpoints</ListItem>
-          </UnorderedList>
+          <Markdown />
         </Container>
       </main>
     </>
