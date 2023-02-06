6f59c5db05beb57bd82a2858c3ecf909bfb8d3be
Gibsol <gibsol75@gmail.com>
2023-02-06 19:02:16 +0200

    :coffin: Removed Navbar from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
-import Navbar from "../components/Navbar";
-import chakraTheme from "../utils/chakraTheme";
+import chakraTheme from "../styles/chakraTheme";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={chakraTheme}>
-      <Navbar />
       <Box mb={10}>
         <Component {...pageProps} />
       </Box>
