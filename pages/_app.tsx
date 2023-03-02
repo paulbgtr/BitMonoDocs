1980635483d727a18890792f7beed9626ff6378e
Gibsol <gibsol75@gmail.com>
2023-03-02 18:10:48 +0200

    :fire: Wipe the project and restarting
    Decide to restart using different styling libraries and different
    approach to the UI.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,8 @@ import "../styles/globals.css";
 
 import { AppProps } from "next/app";
 
-import { Box, ChakraProvider } from "@chakra-ui/react";
-
-import chakraTheme from "../styles/chakraTheme";
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <ChakraProvider theme={chakraTheme}>
-      <Box mb={10}>
-        <Component {...pageProps} />
-      </Box>
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
-
-import { Button, Container, Heading, Text } from "@chakra-ui/react";
-
-import Layout from "../components/Layout";
 
 const Home = () => {
-  const router = useRouter();
   return (
     <>
       <Head>
@@ -19,39 +13,8 @@ const Home = () => {
       </Head>
 
       <main>
-        <Layout>
-          <Container maxWidth={"3xl"} mx={[0, 100]} mt={[50, 100]}>
-            <Heading as={"h1"} fontSize={[50, 64]}>
-              An open-source, free protector for Mono
-            </Heading>
-
-            <Text fontSize={[18, 20]} mt={[6, 7]}>
-              Free open-source protector for Mono, empty decompilers? bits?
-              crashes?! All this and even more is right here!
-            </Text>
-
-            <Button
-              onClick={() => {
-                router.push("/downloads");
-              }}
-              mt={[6, 7]}
-              variant={"light"}
-            >
-              Get Started
-            </Button>
-            <Button
-              onClick={() => {
-                router.replace("https://github.com/sunnamed434/BitMono");
-              }}
-              style={{ border: "2px solid #fff" }}
-              ms={[3, 4]}
-              mt={[6, 7]}
-              variant={"dark"}
-            >
-              Star on GitHub
-            </Button>
-          </Container>
-        </Layout>
+        <h1>Hello</h1>
+        <button className="btn">Hello</button>
       </main>
     </>
   );
