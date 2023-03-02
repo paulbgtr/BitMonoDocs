4c174b8484a0cb41e40747c762da894953218765
Gibsol <gibsol75@gmail.com>
2023-03-02 20:33:16 +0200

    :lipstick: Import fontawersome and wrap the App into Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { AppProps } from "next/app";
 
+import Layout from "../components/Layout";
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default MyApp;
