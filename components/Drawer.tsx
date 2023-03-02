c819acf31d6eabce8fe5e84b066995b608f97d6a
Gibsol <gibsol75@gmail.com>
2023-03-02 20:27:40 +0200

    :lipstick: A Sidebar component.

---
+++ components/Drawer.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Protections from "./Protections";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Drawer = ({ children }: Props) => {
+  return (
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="flex flex-col items-center drawer-content">
+        <label
+          htmlFor="my-drawer-2"
+          className="self-start btn btn-ghost drawer-button lg:hidden"
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </label>
+        {children}
+      </div>
+      <div className=" drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="p-4 bg-black menu w-80 text-base-content">
+          <li>
+            <Link href="/docs">Getting Started</Link>
+          </li>
+          <Protections />
+          <label>Configuration</label>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Drawer;
