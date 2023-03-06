676a89e0ff8f2e6cbd64bcd7526e7fbd92e15c4c
Gibsol <gibsol75@gmail.com>
2023-03-06 10:52:01 +0200

    :fire: Rewrite the docs/index page
    Rewrite the page using gray-matter and remark for parsing the Markdown
    instead of importing it as a component using MDX.

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,36 +1,31 @@
-import Head from "next/head";
-import Link from "next/link";
+import fs from "fs";
+import matter from "gray-matter";
+import { GetStaticProps } from "next";
+import { remark } from "remark";
+import html from "remark-html";
 
-import Markdown from "../../markdown/about.md";
-
-const Home = () => {
-  return (
-    <>
-      <Head>
-        <meta
-          name="description"
-          content="An open-source, free protector for Mono"
-        />
-        <link rel="icon" href="./favicon.ico" />
-      </Head>
-
-      <main>
-        <Markdown />
-
-        <h2 className="mt-3">Installation</h2>
-        <p>
-          You can install the latest version of BitMono{" "}
-          <Link
-            className="link"
-            href="https://github.com/sunnamed434/BitMono/releases"
-          >
-            Here
-          </Link>
-          .
-        </p>
-      </main>
-    </>
-  );
+type Props = {
+  content: string;
+  data: {
+    [key: string]: any;
+  };
 };
 
-export default Home;
+const About = ({ content, data }: Props) => {
+  return <div dangerouslySetInnerHTML={{ __html: content }} />;
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const file = fs.readFileSync(`${process.cwd()}/markdown/about.md`, "utf8");
+
+  const { content, data } = matter(file);
+
+  const processedContent = await remark()
+    .use(html)
+    .process(content);
+  const htmlContent = processedContent.toString();
+
+  return { props: { content: htmlContent, data } };
+};
+
+export default About;
