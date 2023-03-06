260a510aa9cc0055e51618cce890227296b62d6e
Gibsol <gibsol75@gmail.com>
2023-03-06 10:50:09 +0200

    :wrench: Remove MDX from the nextjs config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-module.exports = withMDX({
-  // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-});
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = nextConfig;
