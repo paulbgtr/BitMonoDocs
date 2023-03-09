aba3d524b4b3ef86b62a2620a9a5b0ce45b4cafb
Gibsol <gibsol75@gmail.com>
2023-03-09 17:19:39 +0200

    :technologist: Add the new Hero component to the 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import Hero from "../components/Hero";
+
 const Home = () => {
   return (
     <>
@@ -14,19 +16,17 @@ const Home = () => {
       </Head>
 
       <main>
-        <div className="min-h-screen hero">
-          <div className="text-center hero-content">
-            <div className="max-w-md">
-              <h1 className="text-5xl font-bold">404</h1>
-              <p className="py-6">
-                The page that you are looking for does not exist.
-              </p>
-              <Link href="/docs" className="link">
-                Go Home
-              </Link>
-            </div>
+        <Hero>
+          <div className="text-center">
+            <h1 className="text-5xl font-bold">404</h1>
+            <p className="py-6">
+              The page that you are looking for does not exist.
+            </p>
+            <Link href="/docs" className="link">
+              Go Home
+            </Link>
           </div>
-        </div>
+        </Hero>
       </main>
     </>
   );
